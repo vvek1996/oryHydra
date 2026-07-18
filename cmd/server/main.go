d00cd233ef57764878b6577e5744e52b995496ce
vivekkp <vivekkp@evtaar.com>
2026-07-18 08:12:00 +0530

    ff

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,7 +9,7 @@ func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintln(w, "Service is healthy fff")
+		fmt.Fprintln(w, "Service is healthy1")
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
